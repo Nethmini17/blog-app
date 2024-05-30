0cd139058751648d370adef10df593cdebcc86d5
Nethmini17 <muthuhewaamnk.20@uom.lk>
2024-05-30 20:29:35 +0530

    completed the rest api

--- api/routes/users.js
+++ api/routes/users.js
@@ -18,11 +18,8 @@ router.put("/:id", async (req, res) => {
         },
         { new: true }
       );
-      //if(updatedUser){
+
       res.status(200).json(updatedUser);
-      //     }else{
-      //         res.status(404).json("Account not found!");
-      //    }
     } catch (err) {
       res.status(500).json(err);
     }
@@ -35,25 +32,35 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   if (req.body.userId === req.params.id) {
     try {
-        const user = await User.findById(req.params.id);
+      const user = await User.findById(req.params.id);
       try {
         //delete all posts of the user
         await Post.deleteMany({ username: user.username });
         await User.findByIdAndDelete(req.params.id);
-        //if(updatedUser){
-        res.status(200).json("User has been deleted successfully!");
 
-        //     }else{
-        //         res.status(404).json("Account not found!");
-        //    }
+        res.status(200).json("User has been deleted successfully!");
       } catch (err) {
         res.status(500).json(err);
       }
     } catch (err) {
-        res.status(404).json("User not found!");
+      res.status(404).json("User not found!");
     }
   } else {
     res.status(401).json("You can delete only your account!");
   }
 });
+
+//GET USER
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
+
+
